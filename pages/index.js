ddce38ad5a6b82b0dc9202bc33bf4aa8599ff64a
Fukubara <60829788+Fukubara@users.noreply.github.com>
2021-01-26 13:09:27 -0300

    Aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
-import styled from 'styled-components'
+import db from '../db.json'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Background from '../src/components/Background'
+import Widget from '../src/components/Widget'
+import QuizContainer from '../src/components/QuizContainer'
+import QuizLogo from '../src/components/QuizLogo'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Background>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            {db.title}
+          </Widget.Header>
+          <Widget.Content>
+            {db.description}
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da Galera</h1>
+            <p>entrada</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner />
+    </Background>
+  )
 }
+
